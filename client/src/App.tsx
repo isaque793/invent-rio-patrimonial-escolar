28b76c8cdd4470c561bc352db1b27702014da67e
isaque793 <isaquejoabe178@gmail.com>
2026-09-11 20:46:01 -0300

    feat: adicionar rota de login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import AdminUsers from "./pages/AdminUsers";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Management from "./pages/ManagementExcel";
 import ManagementReview from "./pages/ManagementReview";
 import NotFound from "./pages/NotFound";
@@ -15,5 +16,5 @@ import NotFound from "./pages/NotFound";
 function ManagementRoute() { return <AdminGuard><Management /></AdminGuard>; }
 function ManagementReviewRoute() { return <AdminGuard><ManagementReview /></AdminGuard>; }
 function AdministratorsRoute() { return <AdminGuard><AdminUsers /></AdminGuard>; }
-function Router() { return <DashboardLayout><Switch><Route path="/" component={Home} /><Route path="/gestao" component={ManagementRoute} /><Route path="/gestao/analise/:cycleId" component={ManagementReviewRoute} /><Route path="/administradores" component={AdministratorsRoute} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch></DashboardLayout>; }
+function Router() { return <DashboardLayout><Switch><Route path="/" component={Home} /><Route path="/login" component={Login} /><Route path="/gestao" component={ManagementRoute} /><Route path="/gestao/analise/:cycleId" component={ManagementReviewRoute} /><Route path="/administradores" component={AdministratorsRoute} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch></DashboardLayout>; }
 export default function App() { return <ErrorBoundary><ThemeProvider defaultTheme="light"><TooltipProvider><Toaster richColors position="top-right" /><Router /></TooltipProvider></ThemeProvider></ErrorBoundary>; }
